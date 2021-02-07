4c98f551bb4660d2551d68f5e44844e017798e9d
Ivan Grishin <r@exah.me>
2021-02-07 18:58:10 +0000

    🔧 Build library with `babel`
    Required for component selectors in `@emotion/styled`

--- packages/forest/babel.config.cjs
+++ packages/forest/babel.config.cjs
@@ -1,9 +1,22 @@
-// This file only needed for Jest, until native support for ESM modules is shipped
+const isTest = process.env.NODE_ENV === 'test'
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    [
+      '@babel/preset-env',
+      isTest
+        ? { targets: { node: 'current' } }
+        : {
+            modules: false,
+            targets: '> 1%',
+            loose: true,
+          },
+    ],
     ['@babel/preset-react', { runtime: 'automatic' }],
     '@babel/preset-typescript',
   ],
+  plugins: [
+    ['@emotion/babel-plugin', { sourceMap: false, autoLabel: 'never' }],
+  ],
+  ignore: isTest ? [] : ['**/*.test.tsx'],
 }
