1d7b3c9858478f72be2754f90a442e99e0020a68
Ivan Grishin <r@exah.me>
2021-02-07 18:59:22 +0000

    🔧 Add layout outline toggle to code playground editor

--- docs/.internal/code-playground.tsx
+++ docs/.internal/code-playground.tsx
@@ -2,7 +2,8 @@ import * as React from 'react'
 import * as PinyForest from 'piny-forest/src'
 import GithubTheme from 'prism-react-renderer/themes/github'
 import { LiveProvider, LivePreview, LiveEditor, LiveError } from 'react-live'
-import { Box, HStack } from 'piny-forest/src'
+import { useState } from 'react'
+import { Box, Label, HStack } from 'piny-forest/src'
 
 interface PlaygroundProps {
   code: string
@@ -10,22 +11,48 @@ interface PlaygroundProps {
 
 export const SCOPE = { ...React, ...PinyForest }
 
-export const CodePlayground = (props: PlaygroundProps) => (
-  <LiveProvider
-    scope={SCOPE}
-    transformCode={(input) =>
-      /^(function|class)/.test(input) ? input : `<>${input}</>`
-    }
-    {...props}
-  >
-    <HStack gap={4} mb={5}>
-      <Box w={0.5} variant={{ '> div': 'code' }}>
-        <LiveEditor theme={GithubTheme} style={{ backgroundColor: null }} />
-      </Box>
-      <Box w={0.5} variant={{ '> div': 'code' }}>
-        <LivePreview />
-      </Box>
-    </HStack>
-    <LiveError />
-  </LiveProvider>
-)
+export function CodePlayground({
+  code: defaultCode,
+  ...rest
+}: PlaygroundProps) {
+  const [code, setCode] = useState<string>(defaultCode)
+  const [hasOutline, setOutline] = useState<boolean>(false)
+  return (
+    <LiveProvider
+      scope={SCOPE}
+      transformCode={(input) =>
+        /^(function|class)/.test(input) ? input : `<>${input}</>`
+      }
+      code={code}
+      {...rest}
+    >
+      <HStack gap={4} mb={5}>
+        <Box w={0.5} variant={{ '> div': 'code' }}>
+          <LiveEditor
+            theme={GithubTheme}
+            style={{ backgroundColor: null }}
+            code={code}
+            onChange={setCode}
+          />
+        </Box>
+        <Box
+          w={0.5}
+          pss={{
+            '> div': { outline: hasOutline ? '1px solid red' : 'none' },
+          }}
+        >
+          <LivePreview />
+          <Label mt={2}>
+            <input
+              type="checkbox"
+              checked={hasOutline}
+              onChange={(event) => setOutline(event.target.checked)}
+            />
+            {' Outline'}
+          </Label>
+        </Box>
+      </HStack>
+      <LiveError />
+    </LiveProvider>
+  )
+}
